fa0e35a64ca72a5bc64ffeea03f5df2b1abd1e52
SatoNightingale <davidmederosprieto@gmail.com>
2025-12-19 00:48:38 -0500

    Otro commit mas

--- src/main/java/com/satoshihans/practicalaboralsql/PracticalaboralsqlApplication.java
+++ src/main/java/com/satoshihans/practicalaboralsql/PracticalaboralsqlApplication.java
@@ -8,7 +8,7 @@ public class PracticalaboralsqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticalaboralsqlApplication.class, args);
-		System.out.println("Hello world");
+		System.out.println("Prueba");
 	}
-
+	
 }
